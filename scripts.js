321fea1f74cc036fbfbfff62829ba2a25f935b4e
taranbeer singh <62498540+staranbeer@users.noreply.github.com>
2021-02-17 13:40:26 +0530

    changed the z-index

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ let cross = document.getElementById('cross')
 let clicked = false
 form.style.opacity = "0"
 cross.style.opacity  = "0"
+
 button.addEventListener('click', ()=>{
    form.style.opacity = "1"
    button.style.opacity = "0"
